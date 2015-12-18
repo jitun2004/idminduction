8d1690686234fb40e3c7e05cb0afb28d3414df3d
Ranjit <jitun2004@gmail.com>
2015-12-18 20:32:06 +0530

    ProgressLogic

--- progress/progress_update.php
+++ progress/progress_update.php
@@ -14,18 +14,30 @@ $hostname = "localhost";
 $dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 $selected = mysql_select_db("microfocus",$dbhandle);
 $result = mysql_query("select COUNT(*) from user_progress where user='".$user."' and component='".$component."'");
+$count = mysql_result($result,0);
 
-if ($result[0]==0 && $section=='what'){
-	echo $result[0];
+if ($count==0 && $section=='what'){
+	echo "If ";
 	$sql= "insert into user_progress(user,component,what) values ('".$user."','".$component."','T')";
 	$query=mysql_query($sql);
 	$result1=mysql_fetch_array($query);
 	
 }
-else{
-	
-	echo "select COUNT(*) from user_progress where user='".$user."' and component='".$component."'<br>";
-	//echo $result['user'].">>".$result['component'];
+elseif($count==1 && $section=='what'){
+	$sql1="select * from user_progress where user='".$user."' and component='".$component."'";
+	$query1=mysql_query($sql1);
+	$result1=mysql_fetch_array($query1);
+	if($result1['what']=='F'){
+		$sql= "UPDATE user_progress SET what='T' where user='".$user."' and component='".$component."'";
+		$query=mysql_query($sql);
+		$result2=mysql_fetch_array($query);
+	}
+	else{
+		$sql= "UPDATE user_progress SET what='F' where user='".$user."' and component='".$component."'";
+		$query=mysql_query($sql);
+		$result2=mysql_fetch_array($query);
+	}
+
 }
 
 ?>
